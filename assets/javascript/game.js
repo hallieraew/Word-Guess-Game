52e28b38ac0fdfc8d78602b6f9aa848135037fb9
hallieraew <hallieweintraub@gmail.com>
2019-12-13 12:12:16 -0600

    start to psuedo

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Create an array that lists out all options of the alphabet
+
+// Create an array of possible words to guess - is there something for a random selector for the computer to pick a word?
+
+// Create variables for win
+
+//Create variables for guess left
+
+// Create variables for letters guessed so far
+
+//if statements for if user guessses word to randomly select another
