220e8e5e935ecd823c9f6456f818cb5ed7a264a1
hallieraew <hallieweintraub@gmail.com>
2019-12-13 15:22:17 -0600

    a first attempt at code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,22 @@
+var userChoice = [];
 // Create an array that lists out all options of the alphabet
 
 // Create an array of possible words to guess - is there something for a random selector for the computer to pick a word?
-
+var win = 0;
+var guess = 0;
+var guessLeft = 0;
 // Create variables for win
 
 //Create variables for guess left
 
 // Create variables for letters guessed so far
 
+// Create reference to text in html
+
+var directions = document.getElementById("directions");
+var winText = document.getElementById("win-text");
+var guessesLeft = document.getElementById("guesses-left");
+var guesses = document.getElementById("guesses");
+
 //if statements for if user guessses word to randomly select another
+
