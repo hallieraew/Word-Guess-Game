96db3aeaafb99d99d13ab3000b9cb2c8224c4b4e
hallieraew <hallieweintraub@gmail.com>
2019-12-15 17:56:39 -0600

    re-pseudo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,29 @@
-var userChoice = [];
-// Create an array that lists out all options of the alphabet
+var words = ["cow", "pig", "goat", "chicken", "rooster", "dog", "cat", "donkey", "horse"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Create an array of possible words to guess - is there something for a random selector for the computer to pick a word?
 var win = 0;
-var guess = 0;
+var lettersGuessed = 0;
 var guessLeft = 0;
-// Create variables for win
 
-//Create variables for guess left
+// create function for letter press
 
-// Create variables for letters guessed so far
 
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var wordSelect = words[Math.floor(Math.random() * words.length)];
+    console.log(wordSelect);
+    var correctLetters = "";
+    document.getElementById("currentGuess").innerHTML = blanks(words)
+    
+    function blanks(words) {
+        for (var i = 0; i < words.length; i++) {
+            correctLetters += ("_ ");
+        }
+        return correctLetters;
+    
+};
+}
 // Create reference to text in html
 
 var directions = document.getElementById("directions");
@@ -18,5 +31,7 @@ var winText = document.getElementById("win-text");
 var guessesLeft = document.getElementById("guesses-left");
 var guesses = document.getElementById("guesses");
 
-//if statements for if user guessses word to randomly select another
+document.getElementById("current-word").innerHTML = wordSelect;
 
+
+// if statements for if user guessses word to randomly select
---
+++ assets/javascript/game2.js
@@ -0,0 +1,14 @@
+// list of words to guess
+// list of alphabet letters
+// number of times a user can guess before end of game
+// list letters guessed so far
+// document to innerHTML for each field which needs to pull in value: i.e. win, loss, letters, guesses left
+// function for user input on keyboard
+// docment to uppercase letters
+// input needs to recognize all lower case to match array of letter options
+//need to hide the word until the letters are guessed
+// need to replace the empty space when correct letter is guessed 
+// once reach maximum number of guesses report loss    
+    // add tally to losses
+    // reset for a new word
+// add to wins when user guesses correctly
