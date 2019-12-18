2de745b7c3b0894cf740713b7ce3f4d25bfd76cd
hallieraew <hallieweintraub@gmail.com>
2019-12-18 16:10:12 -0600

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,15 @@
 var words = ["cow", "pig", "goat", "chicken", "rooster", "dog", "cat", "donkey", "horse"];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Create an array of possible words to guess - is there something for a random selector for the computer to pick a word?
+
 var win = 0;
 var loss = 0;
-var lettersGuessed = 0;
+var lettersGuessed = [];
 var guessLeft = 15;
 var correctLetters = [];
 var yourGuess = [];
 var inProgress = false;
+
 
 // create function for letter press
 var wordSelect = "";
@@ -17,41 +18,59 @@ document.onkeyup = function (event) {
 
     if (inProgress) {
 
+        var gameWon = true;
         document.getElementById("guesses").innerHTML = event.key;
         var yourGuess = event.key;
+        var letterFound = false;
 
         for (var i = 0; i < wordSelect.length; i++) {
             if (wordSelect[i] === yourGuess) {
-                correctLetters[i] = yourGuess
+            correctLetters[i] = yourGuess
+            letterFound = true;
+            }
+            if (correctLetters[i] === ("_")) {
+                gameWon = false;
             }
         }
-        
+        if (letterFound === false) {
+            lettersGuessed.push(yourGuess)
+        }
 
+        if (gameWon) {
+            win++;
+            reset();
+        }
+        else if (guessLeft > 0) {
+            guessLeft--;
+        }
+        else {
+            loss++;
+            reset();
+        }
     }
     else {
-        
+
         wordSelect = words[Math.floor(Math.random() * words.length)];
         console.log(wordSelect);
         inProgress = true;
-        
+
         for (var i = 0; i < wordSelect.length; i++) {
             console.log(wordSelect)
             correctLetters[i] = ("_");
         }
-        
-        if (yourGuess === wordSelect) {
-            wins++;
 
-        }
-        else if (yourGuess !== wordSelect) {
-            guessLeft--;
-        }
-        else if (guessLeft === 0) {
-            loss++;
-        }
-        // document.getElementById("current-word").innerHTML = (correctLetters);
     };
     document.getElementById("current-word").innerHTML = correctLetters;
+
+    // document.getElementById("button").addEventListener("click", reset());
+
+    function reset() {
+        inProgress = false;
+        guessLeft = 15;
+        yourGuess = [];
+        lettersGuessed = [];
+        correctLetters = [];
+    }
 
     // Create reference to text in html
 
@@ -59,8 +78,7 @@ document.onkeyup = function (event) {
     document.getElementById('win-text').innerHTML = "Wins: " + win;
     document.getElementById('loss-text').innerHTML = "Losses: " + loss;
     document.getElementById('guesses-left').innerHTML = "Guesses left: " + guessLeft;
-    document.getElementById('guesses').innerHTML = "Letters already guessed: " + yourGuess;
-
+    document.getElementById('guesses').innerHTML = "Letters already guessed: " + lettersGuessed;
 
 };
 
